9634cb224e56c35468b379675d661b7a9246a2ee
Frank Deming <frank.deming@icloud.com>
2026-06-25 15:01:48 -0600

    feat: Setup CMake build system and core dependencies

---
+++ src/precomp.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include <cmath>
+#include <gsl/gsl>
+#include <cstdlib>
+#include <cstdint>
+#include <GLFW/glfw3.h>
+#include <string>
+#include <string_view>
+#include <glm/glm.hpp>

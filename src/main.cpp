e01abf5ca285ce6c28b3936327d103bcfe82f632
Frank Deming <frank.deming@icloud.com>
2026-06-25 15:01:50 -0600

    feat: Add basic application entry point and event loop

---
+++ src/main.cpp
@@ -0,0 +1,37 @@
+#include <GLFW/glfw3.h>
+#include "glfw_initialization.h"
+#include "glfw_monitor.h"
+#include "glfw_window.h"
+#include <precomp.h>
+
+
+std::int32_t main(std::int32_t argc, gsl::zstring* argv[]) {
+
+    const veng::GLFWInitialization _glfw;
+
+    veng::Window window("Vulkan Engine",
+                        {800, 600});
+
+    //gsl::final_action _cleanup_window([&window] {
+        //glfwDestroyWindow(window.GetHandle());
+    //});
+
+    // Get the list of monitors and their count
+    //
+    gsl::span<GLFWmonitor*> monitors = veng::GetMonitors();
+
+    if (monitors.size() > 1) {
+        veng::MoveWindowToMonitor(window.GetHandle(), monitors[1]);
+    }
+
+    window.TryMoveToMonitor(1);
+
+    
+    // Main loop
+    //
+    while (!glfwWindowShouldClose(window.GetHandle())) {
+        glfwPollEvents();
+    }
+
+    return EXIT_SUCCESS;
+}

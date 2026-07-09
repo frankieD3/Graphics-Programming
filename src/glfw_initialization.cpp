9b49699bed065d928342de57691aa2c434cf881a
Frank Deming <frank.deming@icloud.com>
2026-07-09 15:41:35 -0600

    feat(glfw): Use spdlog for GLFW error logging

--- src/glfw_initialization.cpp
+++ src/glfw_initialization.cpp
@@ -1,12 +1,24 @@
 #include "precomp.h"
 #include <GLFW/glfw3.h>
 #include <glfw_initialization.h>
+#include "spdlog/spdlog.h"
 
 namespace veng {
+
+    void glfw_error_callback(std::int32_t error_code,
+                             const char* description) 
+    {
+        spdlog::error("GLFW Error ({}): {}", error_code, description); 
+           
+    }
+
     
     GLFWInitialization::GLFWInitialization() {
+
+        glfwSetErrorCallback(glfw_error_callback);
+
         if(glfwInit() == GLFW_FALSE) {
-            std::exit(EXIT_FAILURE);
+            //std::exit(EXIT_FAILURE);
         }
     }
 

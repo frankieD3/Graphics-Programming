28cdb7c9b515cdb202b5d1a24ccc6f762e5f21f8
Frank Deming <frank.deming@icloud.com>
2026-08-20 20:27:08 -0600

    feat: Use vertex and index buffers for rendering in main application

--- src/main.cpp
+++ src/main.cpp
@@ -24,16 +24,38 @@ std::int32_t main(std::int32_t argc, gsl::zstring* argv[]) {
 
     window.TryMoveToMonitor(1);
 
+    std::array<veng::Vertex, 3> vertices = {
+        veng::Vertex{glm::vec3{0.0f, -0.5f, 0.0f}, glm::vec3{1.0f, 0.0f, 0.0f}},
+        veng::Vertex{glm::vec3{0.5f, 0.5f, 0.0f}, glm::vec3{0.0f, 1.0f, 0.0f}},
+        veng::Vertex{glm::vec3{-0.5f, 0.5f, 0.0f}, glm::vec3{0.0f, 0.0f, 1.0f}}
+    };
+
+    veng::BufferHandle vertex_buffer = graphics.CreateVertexBuffer(vertices);
+
+    std::array<std::uint32_t, 3> indices = { 0, 1, 2 };
+
+    veng::BufferHandle index_buffer = graphics.CreateIndexBuffer(indices);
+
     
     // Main loop
     //
     while (!glfwWindowShouldClose(window.GetHandle())) {
         glfwPollEvents();
         if (graphics.BeginFrame()) {
-            graphics.RenderTriangle();
+            //graphics.RenderTriangle();
+            //graphics.RenderBuffer(vertex_buffer,
+            //                      static_cast<std::uint32_t>(vertices.size()));
+            graphics.RenderIndexBuffer(vertex_buffer,
+                                       index_buffer,
+                                       static_cast<std::uint32_t>(indices.size()));
             graphics.EndFrame();
         }
     }
+
+    vkDeviceWaitIdle(graphics.GetLogicalDevice());
+
+    graphics.DestroyBuffer(vertex_buffer);
+    graphics.DestroyBuffer(index_buffer);
 
     return EXIT_SUCCESS;
 }
